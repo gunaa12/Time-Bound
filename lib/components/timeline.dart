55440d30c994c08d3a2ccadccc4f42a6163c3290
Guna <gunavikasavula@gmail.com>
2023-01-22 05:37:05 -0500

    almost everything working

--- lib/components/timeline.dart
+++ lib/components/timeline.dart
@@ -7,60 +7,43 @@ import 'package:time_bound/constants.dart';
 import 'package:time_bound/components/deadline_card.dart';
 import 'package:time_bound/components/indicator.dart';
 
-class CustomTimeline extends StatefulWidget {
-  const CustomTimeline({super.key});
-
-  @override
-  State<StatefulWidget> createState() => _CustomTimeline();
-}
-
-class _CustomTimeline extends State<CustomTimeline> {
+class CustomTimeline extends StatelessWidget {
   late List<TimelineEventDisplay> events;
 
-  @override
-  void initState() {
-    List<String> dbEntries = ["CS 352", "CS 362", "CS 405", "CS 468"];
-    super.initState();
-    events = [
-      generateEvent("CS 381"),
-      generateEvent("CS 354"),
-    ];
+  CustomTimeline({required this.events});
 
-    for(int i = 0; i <dbEntries.length; i++) {
-      events.add(generateEvent(dbEntries.elementAt(i)));
-    }
-
-
-    // int dbSize = 10;
-    // String dbEntry = "DATABASE ENTRY";
-    // for(int i = 0; i < dbSize; i++) {
-    //   events.add(generateEvent(dbEntry));
-    // }
-
-  }
+  // @override
+  // void initState() {
+  //   List<String> dbEntries = ["CS 352", "CS 362", "CS 405", "CS 468"];
+  //   events = [
+  //     generateEvent("CS 381"),
+  //     generateEvent("CS 354"),
+  //   ];
+  //
+  //   for(int i = 0; i <dbEntries.length; i++) {
+  //     events.add(generateEvent(dbEntries.elementAt(i)));
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    //retrieve data from database
-
     return _buildTimeline();
-
   }
 
-  TimelineEventDisplay generateEvent(String course_name) {
-    return TimelineEventDisplay(
-      child: DeadlineCard(
-        course_name: course_name,
-        deadlines: Container(),
-        color: kOrange,
-        date: "Jan 22",
-      ),
-      indicatorSize: 56,
-      indicator: Indicator(
-          display_icon: Icons.access_alarms_rounded,
-      ),
-    );
-  }
+  // TimelineEventDisplay generateEvent(String course_name) {
+  //   return TimelineEventDisplay(
+  //     child: DeadlineCard(
+  //       course_name: course_name,
+  //       deadlines: Container(),
+  //       color: kOrange,
+  //       date: "Jan 22",
+  //     ),
+  //     indicatorSize: 56,
+  //     indicator: Indicator(
+  //         display_icon: Icons.access_alarms_rounded,
+  //     ),
+  //   );
+  // }
 
   Widget _buildTimeline() {
     return TimelineTheme(
--- lib/screens/student_screen.dart
+++ lib/screens/student_screen.dart
@@ -1,10 +1,16 @@
 // Imports
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_timeline/event_item.dart';
 import 'package:time_bound/components/header.dart';
 import 'package:time_bound/components/button.dart';
 import 'package:time_bound/components/timeline.dart';
 import 'package:time_bound/constants.dart';
 import 'package:time_bound/screens/join_course_screen.dart';
+import '../components/deadline_card.dart';
+import '../components/indicator.dart';
 
 class StudentScreen extends StatefulWidget {
   static const String id = 'student_screen_id';
@@ -14,35 +20,91 @@ class StudentScreen extends StatefulWidget {
 }
 
 class _StudentScreenState extends State<StudentScreen> {
+  late FirebaseFirestore _db;
+  late FirebaseAuth _auth;
+  late var timeline = CustomTimeline(events: [TimelineEventDisplay(child: Container())]);
+
+  @override
+  void initState() {
+    _db = FirebaseFirestore.instance;
+    _auth = FirebaseAuth.instance;
+    generateTimeline();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: kBackgroundColor,
-        body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Header(),
-                Expanded(
-                  child: CustomTimeline()
-                ),
-                Center(
-                  child: Button(
-                    content: Text(
-                      "Join Course",
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Header(),
+              Expanded(
+                child: timeline
+              ),
+              Center(
+                child: Button(
+                  content: Text(
+                    "Join Course",
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
-                    onPress: () {
-                      Navigator.pushNamed(context, JoinCourseScreen.id);
-                    },
-                    color: kEmerald,
                   ),
-                )
-              ],
-            )
-        ),
-      );
+                  onPress: () {
+                    Navigator.pushNamed(context, JoinCourseScreen.id);
+                  },
+                  color: kEmerald,
+                ),
+              )
+            ],
+          )
+      ),
+    );
+  }
+
+  void generateTimeline() {
+    _db.collection('users').doc(_auth.currentUser?.email).get().then(
+       (res) => generateTimelineHelper(res)
+    );
+  }
+
+  Future<void> generateTimelineHelper(var res) async {
+    var enrolled_courses = res.data()['courses'];
+    var all_courses_deadlines = [];
+    for (String course in enrolled_courses) {
+      var course_doc = await _db.collection('courses').doc(course).get();
+      var deadlines = course_doc.data()!['deadlines'];
+      for (var deadline in deadlines) {
+        deadline['course_name'] = course;
+        all_courses_deadlines.add(deadline);
+      }
+    }
+
+    all_courses_deadlines.sort((a, b) => a['due_time'].compareTo(b['due_time']));
+
+    List<TimelineEventDisplay> deadline_events = [];
+    for (var deadline in all_courses_deadlines) {
+      deadline_events.add(generateEvent(deadline));
+    }
+
+    print(deadline_events);
+
+    timeline = CustomTimeline(events: deadline_events);
+  }
+
+  TimelineEventDisplay generateEvent(var deadline) {
+    return TimelineEventDisplay(
+      child: DeadlineCard(
+        course_name: deadline['course_name'],
+        deadlines: Container(),
+        color: kOrange,
+        date: deadline['due_date'].toString(),
+      ),
+      indicatorSize: 56,
+      indicator: Indicator(
+          display_icon: Icons.access_alarms_rounded,
+      ),
+    );
   }
 }
